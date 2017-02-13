8b7374d988dcb5326672e23f064d244a7ba2188b
Eduard <eddiglad@gmail.com>
2017-02-13 22:57:22 +0300

    add chart

--- public/script.js
+++ public/script.js
@@ -1,8 +1,24 @@
 'use script';
 
-$.get('/sensor', {}, onAjaxSuccess, 'json');
-
-function onAjaxSuccess(data){
+$.get('/sensor', function (data){
 	$('#humidity .value').text(data.h);
 	$('#temp .value').text(data.t);
+});
+
+google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart(){
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Time');
+	data.addColumn('number', 'Humidity');
+	data.addColumn('number', 'Temp');
+
+	$.get('/data', function(d){
+		console.log(`arr from server: \n ${d}`);
+		data.addRows(d);
+
+		var chart = google.visualization.LineChart(document.getElementById('chart'));
+		chart.draw(data, null);
+	});
 }
